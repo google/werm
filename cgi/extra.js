3bc9889ffb5e92bf8ddfd085105ba7969d48d4a9
Matthew DeVore <matvore@pobox.com>
2023-02-19 18:05:59 -0600

    use $WERMDIR in extra.js

--- cgi/extra.js
+++ cgi/extra.js
@@ -7,4 +7,4 @@ hubip=`awk '/myhouse/ { print $1 }' /etc/hosts`
 
 sed "s/{{hubip}}/$hubip/g" \
 	~/gum/werm_gum.js \
-	../local.js		# We don't have $WERMDIR :( so hack with $PWD :)
+	$WERMDIR/local.js

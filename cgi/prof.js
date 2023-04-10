75d5a1d8cfd56b1cb768827fbc676fefe3723e13
Matthew DeVore <matvore@s76>
2023-04-09 19:55:35 -0500

    separate the macro settings from the rest of the program

---
+++ cgi/prof.js
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo Content-type: text/javascript
+echo
+
+echo $QUERY_STRING | (
+echo '
+genjs () {
+	if test -x $1; then
+		$1
+	elif test -e $1; then
+		cat $1
+	fi
+}
+'
+sed '
+s_[^,]*_genjs $WERMDIR/js/\0.js; genjs $HOME/.config/werm/js/\0.js_g
+' ) | /bin/sh

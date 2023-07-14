ed6a367ad96fa8bd925829fa1f556fdaa0b98415
Matthew DeVore <matvore@chromium.org>
2023-07-14 11:32:57 -0500

    move clientpost.js back into engine.js
    It does not need to be separate if we work around the cpp warning.

--- third_party/hterm/engine.js
+++ third_party/hterm/engine.js
@@ -2836,6 +2836,46 @@ hterm.Terminal = function({profileId, storage} = {}) {
                   function() { this.onTerminalReady(); }.bind(this));
 };
 
+/**
+ * Open the selected url.
+ */
+hterm.Terminal.prototype.openSelectedUrl_ = function() {
+  let str = this.getSelectionText();
+
+  // If there is no selection, try and expand wherever they clicked.
+  if (str == null) {
+    this.screen_.expandSelectionForUrl(this.document_.getSelection());
+    str = this.getSelectionText();
+
+    // If clicking in empty space, return.
+    if (str == null) {
+      return;
+    }
+  }
+
+  // Make sure URL is valid before opening. \042 is the ", specified this way to
+  // avoid a cpp warning about an unterminated quote.
+  if (str.length > 2048 || str.search(/[\s[\](){}<>\042'\\^`]/) >= 0) {
+    return;
+  }
+
+  // If the URI isn't anchored, it'll open relative to the extension.
+  // We have no way of knowing the correct schema, so assume http.
+  if (str.search('^[a-zA-Z][a-zA-Z0-9+.-]*://') < 0) {
+    // We have to allow a few protocols that lack authorities and thus
+    // never use the //.  Like mailto.
+    switch (str.split(':', 1)[0]) {
+      case 'mailto':
+        break;
+      default:
+        str = 'http://' + str;
+        break;
+    }
+  }
+
+  hterm.openUrl(str);
+};
+
 hterm.Terminal.prototype.scroll_rows_off = function(cnt)
 {
 	var rows, i, new_len;
